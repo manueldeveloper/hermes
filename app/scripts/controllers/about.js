7213681444ac6b886145c62e75cabd8329887627
Manuel <manueldeveloper@gmail.com>
2018-09-20 22:13:36 +0200

    Generating scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hermesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hermesApp
+ */
+angular.module('hermesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hermesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hermesApp
+ */
+angular.module('hermesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
